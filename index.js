3b503a6d7d626d89932293bcc1cb2ce0658fe6cd
Your Name <shahanricky86@gmail.com>
2023-06-11 18:34:24 +0600

    handle-delete

--- index.js
+++ index.js
@@ -203,6 +203,14 @@ async function run() {
             res.send(result);
         })
 
+        app.delete("/selected-class/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await selectedClassCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
 
         // Connect the client to the server	(optional starting in v4.7)
